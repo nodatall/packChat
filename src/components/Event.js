bb6c77d6c378321e2e5d74cf7695edeba8d909f5
Sylvan Swierkosz <sswierkosz@gmail.com>
2017-03-12 16:36:00 -0700

    Emit events

--- src/components/Event.js
+++ src/components/Event.js
@@ -6,10 +6,20 @@ export default class Event extends Component {
   constructor(props) {
     super(props);
     this.state = { text: 'Parents, my car broke down on the way to Crissy field to pick up Jason. Could one of you bring him home please.' };
+
+    this.handlePress = this.handlePress.bind(this)
+  }
+
+  handlePress() {
+    const { navigator, routes, socket, setCurrentMessage } = this.props
+
+    setCurrentMessage(this.state.text)
+    socket.emit('crisis', {message: this.state.text, packId: 1, childId: 1})
+    navigator.push(routes[6])
   }
 
   render() {
-    const { event, navigator, routes } = this.props
+    const { event } = this.props
 
     return (
       <View style={styles.container}>
@@ -22,7 +32,7 @@ export default class Event extends Component {
           multiline={true}
           placeholder='Enter crisis message'
         />
-        <Button color="red" onPress={() => navigator.push(routes[6])} title="Send Crisis Message"/>
+        <Button color="red" onPress={this.handlePress} title="Send Crisis Message"/>
       </View>
     )
   }
